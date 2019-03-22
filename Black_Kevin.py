74719220f5be57aa9650401afcc7b1845868f6a0
Richard Peña <richardignaciopr@gmail.com>
2019-03-22 17:41:05 -0300

    Initial commit

---
+++ Black_Kevin.py
@@ -0,0 +1,36 @@
+import discord
+import Setup
+
+client = discord.Client()
+'''
+Bot para Stick Horse
+Versión 0.0.1 - Ingreso comandos básicos, puesta en marcha
+Autor: Richard Peña (Vaalus)
+Desarrollado en Python 3.7 usando api Discord.py rewrite
+'''
+@client.event
+async def on_message(message):
+    print(message.content) # Now every message sent will be printed to the console
+
+@client.event
+async def on_message(message):
+    #entrega información de comandos
+    if message.content.find("sh.help") != -1:
+        await message.channel.send("Ayuda Stick Horse - Beta:\n prefijo sh.\n hola = Devuelve tu saludo al estilo Stick Horse \n nh <codigo manga nhentai> = entrega enlace a pagina de nhentai") # If the user says !hello we will send back hi
+
+    #saluda a quien lo salude
+    if message.content.find("sh.hola") != -1:
+        await message.channel.send("Come tierra") # If the user says !hello we will send back hi
+
+    #recibe codigo de nhentai y devuelve link a pagina (no es mucho más lo que se puede hacer, ya que murió su API
+    if message.content.find("sh.nh ") != -1:
+        nh_number = message.content[6:]
+        await message.channel.send("https://nhentai.net/g/{}".format(nh_number))
+
+'''@client.event
+async def on_message(message):
+    if message.content.find("sh.nh ") != -1:
+        nh_number = message.content[6:]
+        await message.channel.send("https://nhentai.net/g/{}".format(nh_number))'''
+
+client.run(Setup.token)
