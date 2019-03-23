69a6ae881db84e4608190ccac1a5e7caac07922d
Richard Peña <richardignaciopr@gmail.com>
2019-03-23 14:02:09 -0300

    Help fix - nhrandom command
    >Se mejora ventana de ayuda.
    >Se agrega comando nhrandom

--- Black_Kevin.py
+++ Black_Kevin.py
@@ -1,5 +1,6 @@
 import discord
 import Setup
+import random
 
 '''
 Bot para Stick Horse
@@ -16,8 +17,18 @@ async def on_message(message):
 @client.event
 async def on_message(message):
     #entrega información de comandos
-    if message.content.find("sh.help") != -1:
-        await message.channel.send("Ayuda Stick Horse - Beta:\n prefijo sh.\n hola = Devuelve tu saludo al estilo Stick Horse \n nh <codigo manga nhentai> = entrega enlace a pagina de nhentai") # If the user says !hello we will send back hi
+    if message.content == "sh.help":
+        embed = discord.Embed(title="Ayuda", description="prefijo sh. - Comandos variados")
+        embed.add_field(name="hola", value="Te manda a comer tierra")
+        embed.add_field(name="anime", value="Aun en desarrollo, te dará información de un anime solicitado")
+        embed.add_field(name="jojos", value="Aun en desarrollo (a petición del público), Jojokes")
+        await message.channel.send(content=None, embed=embed)
+
+    if message.content == "sh.help":
+        embed = discord.Embed(title="Comandos NSFW", description="prefijo sh. - Comandos variados")
+        embed.add_field(name="nh <codigo del manga>", value="Devuelve el link para visualizar esa obra de arte")
+        embed.add_field(name="nhrandom", value="Genera un link aleatorio de nhentai")
+        await message.channel.send(content=None, embed=embed)
 
     #saluda a quien lo salude
     if message.content.find("sh.hola") != -1:
@@ -33,9 +44,16 @@ async def on_message(message):
     elif message.content.find("sh.nh ") != -1 and not message.channel.is_nsfw():
         await message.channel.send("No sea marrano y pregunte en un canal NSFW")
 
+    if message.content.find("sh.nhrandom") != -1 and message.channel.is_nsfw():
+        #Genera un numero random que será utilizado como indice de busqueda en nhentai
+        #Este numero es el maximo posible al día 23 de marzo del 2019.
+        nhrnd = random.randint(1, 266859)
+        #Recibe información del manga, rescata el codigo y genera el enlace al manga.
+        await message.channel.send("https://nhentai.net/g/{}".format(nhrnd))
+    #En caso de que esté en un canal SFW da está respuesta
+    elif message.content.find("sh.nhrandom") != -1 and not message.channel.is_nsfw():
+        await message.channel.send("No sea marrano y pregunte en un canal NSFW")
 
-
-    
     if message.content.find("sh.anime ") != -1:
         #tag = on_message[9:]
         await message.channel.send("desarrollo en proceso")
@@ -48,10 +66,6 @@ async def on_message(message):
         #    await message.channel.send("Yare yare daze ")
         #if a == 2:
         await message.channel.send("desarrollo en proceso")
-
-
-
-
 
 
 client.run(Setup.token)
