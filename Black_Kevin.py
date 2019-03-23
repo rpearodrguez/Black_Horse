50ba87684e89b9b07c256853f2c4d8933c1a5248
Richard Peña <richardignaciopr@gmail.com>
2019-03-23 04:35:29 -0300

    Se comentaron algunos comandos en desarrollo
    Para no interferir con el funcionamiento del bot.

--- Black_Kevin.py
+++ Black_Kevin.py
@@ -1,7 +1,7 @@
 from random import randint
 import discord
 import Setup
-import myanimelist
+
 
 
 client = discord.Client()
@@ -35,18 +35,19 @@ async def on_message(message):
         await message.channel.send("No sea marrano y pregunte en un canal NSFW")
 
 
-    '''
+    
     if message.content.find("sh.anime ") != -1:
-        tag = on_message[9:]
-        await message.channel.send(myanimelist.anime.Anime(tag))
+        #tag = on_message[9:]
+        await message.channel.send("desarrollo en proceso)
+        
     
     #frases random de jojos
     if message.content.find("sh.jojos") != -1:
-        a = randint(1, 5)
-        if a == 1:
-            await message.channel.send("Yare yare daze ")
-        if a == 2:
-            await message.channel.send("Come tierra") # If the user says !hello we will send back hi'''
+        #a = randint(1, 5)
+        #if a == 1:
+        #    await message.channel.send("Yare yare daze ")
+        #if a == 2:
+        await message.channel.send("desarrollo en proceso")
 
 
 
