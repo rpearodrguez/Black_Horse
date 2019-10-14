ba149e3d924c527f1e0887bed1fa169d15816060
Richard Peña <richardignaciopr@gmail.com>
2019-10-14 03:50:02 -0300

    Corrección error comando sh.steam
    Se corrige condicional

--- Black_Kevin.py
+++ Black_Kevin.py
@@ -123,7 +123,7 @@ async def on_message(message):
 
         try:
             #"Portada", "Nombre", "Descripcion", "Desarrollador", "Lanzamiento", "Genero", "Metacritic", "Precio"
-            if resultado[1] != "Nombre" and resultado[2] == "Descripcion" and resultado[3] == "Desarrollador" and resultado[4] == "Lanzamiento" and resultado[5] == "Genero" and resultado[6] == "Metacritic" and resultado[7] != "Precio":
+            if resultado[0] != "Portada" and resultado[1] == "Nombre" and resultado[2] == "Descripcion" and resultado[3] == "Desarrollador" and resultado[4] == "Lanzamiento" and resultado[5] == "Genero" and resultado[6] == "Metacritic" and resultado[7] != "Precio":
                 await message.channel.send(resultado[1])
             
             elif resultado[1] != "Nombre" or resultado[2] != "Descripcion" or resultado[3] != "Desarrollador" or resultado[4] != "Lanzamiento" or resultado[5] != "Genero" or resultado[6] != "Metacritic" or resultado[7] != "Precio":
