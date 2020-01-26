9143a2e823aa2f3dbb820b6662f635abd2191b4c
Richard Peña <richardignaciopr@gmail.com>
2020-01-26 03:43:38 -0300

    Retest
    Retest

--- Setup.py
+++ Setup.py
@@ -1,7 +1,2 @@
-import discord
-import asyncio
-from discord.ext import commands
 
-token = "NTU4MTAyNjY1Njk1OTg1Njc0.Xi0nUQ.KxNGmZbCVXd6Z1MCpqTQ-9Azkj4"
-kitsu_client_id = 'dd031b32d2f56c990b1425efe6c42ad847e7fe3ab46bf1299f05ecd856bdb7dd'
-kitsu_client_secret = '54d7307928f63414defd96399fc31ba847961ceaecef3a5fd93144e960c0e151'
+token = "NTU4MTAyNjY1Njk1OTg1Njc0.D3bNYg.gBOlgDWUp2GSo0Ex0jbFPHJ4FJg"
