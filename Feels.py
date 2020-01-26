8362526645d5a80866319beaf290b35327440b79
Richard Peña <richardignaciopr@gmail.com>
2020-01-26 14:14:22 -0300

    Trabajo con Tenor API
    New Gifs in da haus

--- Feels.py
+++ Feels.py
@@ -1,12 +1,32 @@
+import json
+import os
 import random
 
+import requests
+from boto.s3.connection import S3Connection
+
+
 def reactionImage(feel=""):
+
     imagenes = [""]
     if feel == "escobazo":
         imagenes = ["https://media.giphy.com/media/l2Je4FbOimhxM6mE8/giphy.gif", "https://www.stickhorse.cl/wp-content/uploads/2020/01/SH-Escobazo-2.gif", "https://www.stickhorse.cl/wp-content/uploads/2020/01/SH-Escobazo-3.gif"]
 
     if feel == "abrazo":
-        imagenes = ["https://media.tenor.com/images/9fe95432f2d10d7de2e279d5c10b9b51/tenor.gif"]
+        search_term = "anime+hug"
 
+    if feel !=  "escobazo":
+        url = "https://api.tenor.com/v1/random?key={}&q={}&locale=en_US&contentfilter=off&media_filter=minimal&ar_range=wide&limit=1".format(os.environ.get('TENOR_KEY'), search_term)
+        # open with GET method
+        resp = requests.get(url)
+        # http_respone 200 means OK status
+        if resp.status_code == 200:
+            gifs = json.loads(resp.content)
+            imagenes[0] = gifs["results"][0]["media"][0]["webm"]["url"]
+        else:
+            pass
+            
+        
     randImage = random.randint(0, len(imagenes)-1)
+
     return imagenes[randImage]
