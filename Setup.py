17ea77dcebd12f11880f4140420a09dd4d2b794c
Richard Peña <richardignaciopr@gmail.com>
2020-01-26 02:25:47 -0300

    Update
    Update

--- Setup.py
+++ Setup.py
@@ -2,6 +2,6 @@ import discord
 import asyncio
 from discord.ext import commands
 
-token = "NTU4MTAyNjY1Njk1OTg1Njc0.Xi0dsQ.F7uNo6sOoz70BrHl6aZ9Q93rNs8"
+token = "NTU4MTAyNjY1Njk1OTg1Njc0.Xi0gww.dopupgp20dvXCCEJN8foDCeF1_M"
 kitsu_client_id = 'dd031b32d2f56c990b1425efe6c42ad847e7fe3ab46bf1299f05ecd856bdb7dd'
 kitsu_client_secret = '54d7307928f63414defd96399fc31ba847961ceaecef3a5fd93144e960c0e151'
