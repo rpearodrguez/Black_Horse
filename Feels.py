ebf9eddaf0672864d1e0e1c8235dcb39ed9260eb
Richard Peña <richardignaciopr@gmail.com>
2020-02-01 13:42:44 -0300

    Actualización comandos
    Actualización sh.help y adición sh.bite

--- Feels.py
+++ Feels.py
@@ -65,6 +65,9 @@ def reactionImage(feel=""):
 
     if feel == "lewd":
         search_term = "anime+lewd"
+    
+    if feel == "bite":
+        search_term = "anime+bite"
 
     if feel !=  "escobazo":
         url = "https://api.tenor.com/v1/random?key={}&q={}&locale=en_US&contentfilter=off&media_filter=minimal&ar_range=wide&limit=1".format(os.environ.get('TENOR_KEY'), search_term)
