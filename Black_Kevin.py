98328453c1e28af933dc679a896714d08f0902e3
Richard Peña <richardignaciopr@gmail.com>
2020-02-01 21:36:20 -0300

    Reparación mala declaración hug y escobazo

--- Black_Kevin.py
+++ Black_Kevin.py
@@ -170,7 +170,7 @@ async def on_message(message):
 #Reactions Module
 
     if message.content.find("sh.escobazo ") != -1:
-        victima = message.mentions[0].name.name
+        victima = message.mentions[0].name
         print(message.mentions)
         autor = str(message.author).split("#")[0]
         imagen = Feels.reactionImage("escobazo")
@@ -179,7 +179,7 @@ async def on_message(message):
         await message.channel.send(content=None, embed=embed)
     
     if message.content.find("sh.hug ") != -1:
-        victima = message.mentions[0].name.name
+        victima = message.mentions[0].name
         print(message.mentions[0])
         print(message.author)
         autor = str(message.author).split("#")[0]
