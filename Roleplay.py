fca5d0deaf653e25a1da9b46c57a675c6994faf8
Richard Peña <richardignaciopr@gmail.com>
2020-02-04 18:30:58 -0300

    Update Roleplay.py

--- Roleplay.py
+++ Roleplay.py
@@ -5,25 +5,28 @@ def roll (cant_dados = 1, cant_caras = 20):
 
 def fateroll (cant_dados=1, mod=0, authorName=""):
     try:
-        textopositivo = ""
-        textonegativo = ""
-        contador = 0
-        positivos = 0
-        negativos = 0
-        for x in range(1, (1+int(cant_dados))):
-            result = random.randint(1, 2)
-            if result == 1:
-                textopositivo += "+"
-                contador += 1
-                positivos += 1
-                    
-            if result == 2:
-                textonegativo += "-"
-                contador -= 1
-                negativos += 1
-        resultado = contador+int(mod)
+        if cant_dados<20:
+            textopositivo = ""
+            textonegativo = ""
+            contador = 0
+            positivos = 0
+            negativos = 0
+            for x in range(1, (1+int(cant_dados))):
+                result = random.randint(1, 2)
+                if result == 1:
+                    textopositivo += "+"
+                    contador += 1
+                    positivos += 1
+                        
+                if result == 2:
+                    textonegativo += "-"
+                    contador -= 1
+                    negativos += 1
+            resultado = contador+int(mod)
 
-        finalRoll = "El roll de {} es ({}{}) ({}{}) ({})".format(authorName,textopositivo,textonegativo,contador,mod,resultado)
-        return finalRoll
+            finalRoll = "El roll de {} es ({}{}) ({}{}) ({})".format(authorName,textopositivo,textonegativo,contador,mod,resultado)
+            return finalRoll
+        else:
+            return ("Intentas rolear una cantidad muy grande de dados")
     except:
         return ("Ocurrio un error al intentar rolear")
