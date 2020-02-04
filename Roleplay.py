bcb677ab28b52e0792b028968422edc76dda2acb
Richard Peña <richardignaciopr@gmail.com>
2020-02-04 18:09:23 -0300

    Update Roleplay.py

--- Roleplay.py
+++ Roleplay.py
@@ -4,31 +4,30 @@ def roll (cant_dados = 1, cant_caras = 20):
     pass
 
 def fateroll (cant_dados=1, mod=0, authorName=""):
-    try:
-        textopositivo = ""
-        textonegativo = ""
-        contador = 0
-        positivos = 0
-        negativos = 0
-        modificador = ""
-        if mod > 0:
-            modificador = "+"
-        if mod < 0:
-            modificador = "-"
-        for x in range(1, (1+cant_dados)):
-            result = random.randint(1, 2)
-            if result == 1:
-                textopositivo += "+"
-                contador += 1
-                positivos += 1
+    textopositivo = ""
+    textonegativo = ""
+    contador = 0
+    positivos = 0
+    negativos = 0
+    modificador = ""
+    if mod > 0:
+        modificador = "+"
+    if mod < 0:
+        modificador = "-"
+    for x in range(1, (1+cant_dados)):
+        result = random.randint(1, 2)
+        if result == 1:
+            textopositivo += "+"
+            contador += 1
+            positivos += 1
                 
-            if result == 2:
-                textonegativo += "-"
-                contador -= 1
-                negativos += 1
-        resultado = contador+mod
+        if result == 2:
+            textonegativo += "-"
+            contador -= 1
+            negativos += 1
+    resultado = contador+mod
 
-        finalRoll = "El roll de {} es ({}{}) ({}{}{}) ({})".format(authorName,textopositivo,textonegativo,contador,modificador,mod,resultado)
-        return finalRoll
-    except: 
-        return ("Ocurrio un error al intentar rolear")
+    finalRoll = "El roll de {} es ({}{}) ({}{}{}) ({})".format(authorName,textopositivo,textonegativo,contador,modificador,mod,resultado)
+    return finalRoll
+    
+    #return ("Ocurrio un error al intentar rolear")
