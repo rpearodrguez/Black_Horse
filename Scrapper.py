1b44f32ff6d9400dbe999722e19f14f0bcef10a9
Richard Peña <richardignaciopr@gmail.com>
2020-08-17 01:12:51 -0400

    Intento reparacion bot discord

--- Scrapper.py
+++ Scrapper.py
@@ -33,7 +33,7 @@ def animeScrap(urlb=""):
         try:
             titulo = resultado["data"][resultado_elegido]["attributes"]["canonicalTitle"]
             portada = resultado["data"][resultado_elegido]["attributes"]["posterImage"]["large"]
-            sinopsis = translator.translate(resultado["data"][resultado_elegido]["attributes"]["synopsis"],dest='es').text
+            sinopsis = " ".join("\n".split(translator.translate(resultado["data"][resultado_elegido]["attributes"]["synopsis"],dest='es').text))
             lanzamiento = resultado["data"][resultado_elegido]["attributes"]["startDate"]
             termino = resultado["data"][resultado_elegido]["attributes"]["endDate"]
             terminado = translator.translate(resultado["data"][resultado_elegido]["attributes"]["status"],dest='es').text
